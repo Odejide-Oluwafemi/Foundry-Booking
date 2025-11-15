3e3e00f78dfc91c3ded3b55ede45724139057510
LordDregg <oluwatosinodejide121@gmail.com>
2025-11-15 15:16:27 +0100

    magic numbers re-fix

--- test/BookingTest.t.sol
+++ test/BookingTest.t.sol
@@ -17,29 +17,18 @@ contract BookingTest is Test {
   Booking sBookingContract;
   address USER = makeAddr("user");
 
-  // Named constants to replace magic numbers (vibe & style kept consistent)
-  address private constant ADDRESS_ZERO = address(0);
+  address constant INVALID_ADDRESS = address(0);
+  address immutable I_SENDER = msg.sender;
+  uint256 constant ITEM_ID = 1;
+  string constant ITEM_NAME = "Name";
+  uint256 constant ITEM_QUANTITY = 10;
+  uint256 constant ITEM_PRICE = 1;
+  uint256 constant STARTING_USER_BALANCE = 10 ether;
+  uint256 constant PURCHASE_ITEM_ID= 1;
+  uint256 constant PURCHASE_QUANTITY = 1;
+  uint256 constant INVALID_ITEM_ID = 0;
 
-  uint256 private constant ITEM_ID = 1;
-  uint256 private constant INVALID_ITEM_INDEX = 1;
-  uint256 private constant INVALID_ITEM_ARRAY_INDEX = 0;
-
-  uint256 private constant ZERO = 0;
-  uint256 private constant ONE = 1;
-
-  uint256 private constant INITIAL_ITEM_QUANTITY = 10;
-  uint256 private constant PURCHASE_QUANTITY = 1;
-  uint256 private constant EXCESS_ORDER_QUANTITY = 11;
-
-  uint256 private constant ITEM_PRICE = 1; // price unit used in createItem
-  uint256 private constant PURCHASE_PRICE_ETHER = 1 ether; // price sent with purchase
-
-  uint256 private constant USER_STARTING_BALANCE = 10 ether;
-  uint256 private constant INSUFFICIENT_CREATION_PAYMENT = 0.5 ether;
-
-  uint256 private constant EXPECTED_ITEMS_AFTER_CREATE = 1;
-  uint256 private constant EXPECTED_OWNER_ITEMS_AFTER_PURCHASE = 1;
-
+// uint256 expectedResult = 0;
   function setUp() public {
     vm.startBroadcast();
     sBookingContract = new Booking();
@@ -49,101 +38,105 @@ contract BookingTest is Test {
   }
 
   function testContractIsSet() public view {
-    assert(address(sBookingContract) != ADDRESS_ZERO);
+    assert(address(sBookingContract) != INVALID_ADDRESS);
   }
 
   function testOwnerIsSet() public view {
-    assertEq(sBookingContract.getOwner(), msg.sender);
+    assertEq(sBookingContract.getOwner(), I_SENDER);
   }
 
   function testGetItemQuantityAfterDeployment() public view {
-    // uint256 public constant ITEM_ID = 1;
-    // uint256 public constant EXPECTED_RESULT = 0;
-
-    assertEq(sBookingContract.getItemQuantity(ITEM_ID), ZERO);
+    assertEq(sBookingContract.getItemQuantity(ITEM_ID), 0);
   }
 
   function testGetItemWithInvalidIndex() public {
     vm.expectRevert(Booking__InvalidItemIndex.selector);
-    sBookingContract.getItem(INVALID_ITEM_INDEX);
+    sBookingContract.getItem(0);
   }
 
   function testGetItemByIdAfterDeployment() public view {
-    assertEq(sBookingContract.getItemById(ITEM_ID).id, ZERO);
+    assertEq(sBookingContract.getItemById(ITEM_ID).id, 0);
   }
 
   function testGetOwnerItemsWithInvalidAddress() public {
     vm.expectRevert(Booking__InvalidOwnerAddress.selector);
-    sBookingContract.getOwnerItems(ADDRESS_ZERO);
+    sBookingContract.getOwnerItems(INVALID_ADDRESS);
   }
 
   function testGetOwnerItemsAfterDeployment() public view {
-    assertEq(sBookingContract.getOwnerItems(msg.sender).length, ZERO);
+    uint256 expectedResult = 0;
+    assertEq(sBookingContract.getOwnerItems(I_SENDER).length, expectedResult);
   }
 
   function testGetOwnerItemsByIdAfterDeployment() public view {
-    assertEq(sBookingContract.getOwnerItemById(msg.sender, ITEM_ID).id, ZERO);
+    uint256 expectedResult = 0;
+    assertEq(sBookingContract.getOwnerItemById(I_SENDER, 1).id, expectedResult);
   }
 
   function testGetItemByIdFromItemsArrayAfterDeployment() public view {
-    assertEq(sBookingContract.getOwnerItems(msg.sender).length, ZERO);
+    uint256 expectedResult = 0;
+    assertEq(sBookingContract.getOwnerItems(I_SENDER).length, expectedResult);
   }
 
   modifier itemCreated() {
     vm.prank(USER);
-    vm.deal(USER, USER_STARTING_BALANCE);
-    sBookingContract.createItem{value: sBookingContract.CREATE_ITEM_COST()}("Name", ITEM_PRICE, INITIAL_ITEM_QUANTITY);
+    vm.deal(USER, STARTING_USER_BALANCE);
+    sBookingContract.createItem{value: sBookingContract.CREATE_ITEM_COST()}(ITEM_NAME, ITEM_PRICE, ITEM_QUANTITY);
     _;
   }
 
   modifier itemCreatedAndPurchasedOneQuantity() {
-    sBookingContract.createItem{value: sBookingContract.CREATE_ITEM_COST()}("Name", ITEM_PRICE, INITIAL_ITEM_QUANTITY);
+    sBookingContract.createItem{value: sBookingContract.CREATE_ITEM_COST()}(ITEM_NAME, ITEM_PRICE, ITEM_QUANTITY);
 
     vm.startPrank(USER);
-    vm.deal(USER, USER_STARTING_BALANCE);
-    sBookingContract.purchaseItem{value: PURCHASE_PRICE_ETHER}(ITEM_ID, PURCHASE_QUANTITY);
+    vm.deal(USER, STARTING_USER_BALANCE);
+    sBookingContract.purchaseItem(PURCHASE_ITEM_ID, PURCHASE_QUANTITY);
     vm.stopPrank();
     _;
   }
 
   function testGetItemsArraySize() public itemCreated {
-    assertEq(sBookingContract.getItemsArraySize(), EXPECTED_ITEMS_AFTER_CREATE);
+    uint256 expectedResult = 1;
+    assertEq(sBookingContract.getItemsArraySize(), expectedResult);
   }
 
   function testGetItem() public itemCreated {
-    assertEq(sBookingContract.getItem(ZERO).name, "Name");
+    assertEq(sBookingContract.getItem(0).name, ITEM_NAME);
   }
 
   function testGetItemById() public itemCreated {
-    assertEq(sBookingContract.getItemById(ITEM_ID).name, "Name");
+    assertEq(sBookingContract.getItemById(1).name, ITEM_NAME);
   }
 
   function testGetOwnerItems() public itemCreatedAndPurchasedOneQuantity {
-    assertEq(sBookingContract.getOwnerItems(USER).length, EXPECTED_OWNER_ITEMS_AFTER_PURCHASE);
+    uint256 expectedResult = 1;
+    assertEq(sBookingContract.getOwnerItems(USER).length, expectedResult);
   }
 
   function testGetOwnerItemById() public itemCreatedAndPurchasedOneQuantity {
-    assertEq(sBookingContract.getOwnerItemById(USER, ITEM_ID).name, "Name");
+    assertEq(sBookingContract.getOwnerItemById(USER, 1).name, ITEM_NAME);
   }
 
   function testGetAllItems() public view {
-    assertEq(sBookingContract.getAllItems().length, ZERO);
+    uint256 expectedResult = 0;
+    assertEq(sBookingContract.getAllItems().length, expectedResult);
   }
 
   function testGetItemByIdFromItemsArray() public itemCreated {
-    assertEq(sBookingContract.getItemByIdFromItemsArray(sBookingContract.getAllItems(), ITEM_ID).name, "Name");
+    assertEq(sBookingContract.getItemByIdFromItemsArray(sBookingContract.getAllItems(), 1).name, ITEM_NAME);
   }
 
   function testGetItemRevertsWithInvalidArrayIndex() public {
     vm.expectRevert(Booking__InvalidItemIndex.selector);
-    sBookingContract.getItem(INVALID_ITEM_ARRAY_INDEX);
+    sBookingContract.getItem(0);
   }
 
   function testCreateItemRevertWithZeroQuantity() public {
     vm.startBroadcast();
     
     vm.expectRevert(Booking__ZeroQuantity.selector);
-    sBookingContract.createItem("Name", ITEM_PRICE, ZERO);
+    uint256 itemQuantity = 0;
+    sBookingContract.createItem(ITEM_NAME, ITEM_PRICE, itemQuantity);
     
     vm.stopBroadcast();
   }
@@ -152,87 +145,94 @@ contract BookingTest is Test {
     vm.startBroadcast();
     
     vm.expectRevert(Booking__ZeroPrice.selector);
-    sBookingContract.createItem("Name", ZERO, INITIAL_ITEM_QUANTITY);
+    uint256 itemPriceInEth = 0;
+    sBookingContract.createItem(ITEM_NAME, itemPriceInEth, ITEM_QUANTITY);
     
     vm.stopBroadcast();
   }
 
   function testCreateItemAddsToArray() public itemCreated {
-    assertEq(sBookingContract.getItemsArraySize(), EXPECTED_ITEMS_AFTER_CREATE);
+    uint256 expectedResult = 1;
+    assertEq(sBookingContract.getItemsArraySize(), expectedResult);
   }
 
   function testCreateItemUpdatesQuantityInMapping() public itemCreated {
-    assertEq(sBookingContract.getItemQuantity(ITEM_ID), INITIAL_ITEM_QUANTITY);
+    assertEq(sBookingContract.getItemQuantity(ITEM_ID), ITEM_QUANTITY);
   }
 
   function testCreateItemRevertsWhenInsufficientETHIsSentForItemCreation() public {
     vm.prank(USER);
-    vm.deal(USER, USER_STARTING_BALANCE);
+    vm.deal(USER, STARTING_USER_BALANCE);
     vm.expectRevert(Booking__InsufficientETHSent.selector);
-    sBookingContract.createItem{value: INSUFFICIENT_CREATION_PAYMENT}("Name", ITEM_PRICE, INITIAL_ITEM_QUANTITY);
+    sBookingContract.createItem{value: 0.5 ether}(ITEM_NAME, ITEM_PRICE, ITEM_QUANTITY);
   }
 
   function testPurchaseItemFailsWithInvalidItemId() public itemCreated {
     vm.expectRevert(Booking__InvalidItemIndex.selector);
-    hoax(USER, 10 ether);
-    sBookingContract.purchaseItem{value: PURCHASE_PRICE_ETHER}(INVALID_ITEM_INDEX, INITIAL_ITEM_QUANTITY);
+    sBookingContract.purchaseItem{value: 1 ether}(INVALID_ITEM_ID, ITEM_QUANTITY);
   }
 
   function testPurchaseItemFailsWithZeroQuantity() public itemCreated {
     vm.expectRevert(Booking__ZeroQuantity.selector);
-    sBookingContract.purchaseItem{value: PURCHASE_PRICE_ETHER}(ITEM_ID, ZERO);
+    uint256 itemQuantity = 0;
+    sBookingContract.purchaseItem{value: ITEM_PRICE * 1e18}(ITEM_ID, itemQuantity);
   }
 
   function testPurchaseItemFailsWhenOutOfStock() public {
     vm.startPrank(USER);
-    vm.deal(USER, USER_STARTING_BALANCE);
+    vm.deal(USER, STARTING_USER_BALANCE);
 
-    sBookingContract.createItem{value: sBookingContract.CREATE_ITEM_COST()}("Name", ITEM_PRICE, ONE);
-    sBookingContract.purchaseItem{value: PURCHASE_PRICE_ETHER}(ITEM_ID, PURCHASE_QUANTITY);
+    uint256 itemQuantity = 1;
+    sBookingContract.createItem{value: sBookingContract.CREATE_ITEM_COST()}(ITEM_NAME, ITEM_PRICE, itemQuantity);
+    sBookingContract.purchaseItem{value: ITEM_PRICE * 1e18}(ITEM_PRICE, itemQuantity);
     vm.stopPrank();
     
     vm.expectRevert(Booking__OutOfStock.selector);
-    sBookingContract.purchaseItem{value: PURCHASE_PRICE_ETHER}(ITEM_ID, PURCHASE_QUANTITY);
+    uint256 purchaseQuantity = 2;
+    sBookingContract.purchaseItem{value: ITEM_PRICE * 1e18}(ITEM_PRICE, purchaseQuantity);
   }
 
   function testPurchaseItemFailsWhenExcessQuantityIsOrdered() public itemCreated {
     vm.expectRevert(Booking__ExcessQuantityOrdered.selector);
-    sBookingContract.purchaseItem{value: PURCHASE_PRICE_ETHER}(ITEM_ID, EXCESS_ORDER_QUANTITY);
+    uint256 purchaseQuantity = 11;
+    sBookingContract.purchaseItem{value: ITEM_PRICE * 1e18}(ITEM_PRICE, purchaseQuantity);
   }
 
   function testPurchaseAddsItemToUserItemsArray() public itemCreatedAndPurchasedOneQuantity {
-    // assertEq(sBookingContract.getOwnerItems(USER)[0].id, 1);
-    assertEq(sBookingContract.ownerHasItem(USER, ITEM_ID), true);
+    bool expectedResult = true;
+    assertEq(sBookingContract.ownerHasItem(USER, ITEM_ID), expectedResult);
   }
 
   function testPurchaseIncrementsUserItemQuantity() public itemCreatedAndPurchasedOneQuantity {
-    assertEq(sBookingContract.getOwnerItemQuantity(USER, ITEM_ID), PURCHASE_QUANTITY);
+    uint256 expectedResult = 1;
+    assertEq(sBookingContract.getOwnerItemQuantity(USER, ITEM_ID), expectedResult);
   }
 
   function testPurchaseDeductsItemQuantity() public itemCreatedAndPurchasedOneQuantity {
-    assertEq(sBookingContract.getItemQuantity(ITEM_ID), INITIAL_ITEM_QUANTITY - PURCHASE_QUANTITY);
+    uint256 expectedResult = 9;
+    assertEq(sBookingContract.getItemQuantity(ITEM_ID), expectedResult);
   }
 
   function testPurchaseAddsToContractBalance() public itemCreated {
     uint256 contractBalanceBefore = address(sBookingContract).balance;
 
     vm.prank(USER);
-    vm.deal(USER, USER_STARTING_BALANCE);
-    sBookingContract.purchaseItem{value: PURCHASE_PRICE_ETHER}(ITEM_ID, PURCHASE_QUANTITY);
+    vm.deal(USER, STARTING_USER_BALANCE);
+    sBookingContract.purchaseItem{value: ITEM_PRICE * 1e18}(ITEM_ID, PURCHASE_QUANTITY);
 
     uint256 contractBalanceAfter = address(sBookingContract).balance;
-    assertEq(contractBalanceAfter, contractBalanceBefore + PURCHASE_PRICE_ETHER);
+    assertEq(contractBalanceAfter, contractBalanceBefore + (ITEM_PRICE * 1e18));
   }
 
   function testPurchaseDeductsUserBalance() public itemCreated {
     uint256 userBalanceBefore = address(USER).balance;
 
     vm.prank(USER);
-    vm.deal(USER, USER_STARTING_BALANCE);
-    sBookingContract.purchaseItem{value: PURCHASE_PRICE_ETHER}(ITEM_ID, PURCHASE_QUANTITY);
+    vm.deal(USER, STARTING_USER_BALANCE);
+    sBookingContract.purchaseItem{value: ITEM_PRICE * 1e18}(ITEM_ID, PURCHASE_QUANTITY);
 
     uint256 userBalanceAfter = address(USER).balance;
-    assertEq(userBalanceAfter, userBalanceBefore - PURCHASE_PRICE_ETHER);
+    assertEq(userBalanceAfter, userBalanceBefore - (ITEM_PRICE * 1e18));
   }
 
   function testWithdrawRevertsWhenCalledByNonOwner() public itemCreatedAndPurchasedOneQuantity{
@@ -257,6 +257,7 @@ contract BookingTest is Test {
     vm.prank(sBookingContract.getOwner());
     sBookingContract.withdraw();
     
-    assertEq(address(sBookingContract).balance, ZERO);
+    uint256 expectedResult = 0;
+    assertEq(address(sBookingContract).balance, expectedResult);
   }
 }
